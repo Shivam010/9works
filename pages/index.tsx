5cd3978a82ef8f4df5e95d5b857c4d010a36f712
Shivam Rathore <me@shivamrathore.com>
2022-01-21 03:19:04 +0530

    chore: force dark on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,7 @@ export default function Index() {
 export function getStaticProps() {
     return {
         props: {
-            strictDarkMode: false,
+            strictDarkMode: true,
         },
     };
 }
